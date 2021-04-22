cd5ce0bea2ff4e3f64c406ef169ec9fccc40cd17
Gopal Mahajan <gopalmit2601@gmail.com>
2021-04-22 22:44:21 +0530

    Formulas to get fault is added

--- src/main/java/controller/parameter.java
+++ src/main/java/controller/parameter.java
@@ -25,18 +25,44 @@ class Generator {
 }
 class Fault{
     String typeOfFault;
-    int faultImpedence=0;
+    float faultImpedence=0;
 
     Fault(String typeOfFault){
         this.typeOfFault=typeOfFault;
     }
 
-    public float lineToLine(float emf,float zeroSequenceImpedence,float positiveSequenceImpedence,float negativeSequenceImpedence){
+    public float lineToGround(float emf,float zeroSequenceImpedence,float positiveSequenceImpedence
+            ,float negativeSequenceImpedence){
         float faultCurrent=0;
         emf= (float) (emf/Math.sqrt(3));
         float equivalentImpedence=zeroSequenceImpedence+positiveSequenceImpedence+negativeSequenceImpedence;
-        faultCurrent=emf/equivalentImpedence;
+        faultCurrent=3*(emf/equivalentImpedence);
 
+        return faultCurrent;
+    }
+
+    public double lineToLine(float emf,float positiveSequenceImpedence,float negativeSequenceImpedence){
+        double faultCurrent=0;
+        emf= (float) (emf/Math.sqrt(3));
+        float equivalentImpedence=positiveSequenceImpedence+negativeSequenceImpedence+faultImpedence;
+        faultCurrent=Math.sqrt(3)*(emf/equivalentImpedence);
+
+        return faultCurrent;
+    }
+
+
+    public double lineToLineToGround(float emf,float positiveSequenceImpedence
+            ,float negativeSequenceImpedence, float zeroSequenceImpedence){
+        double faultCurrent=0;
+        emf= (float) (emf/Math.sqrt(3));
+        float imp=(negativeSequenceImpedence*(zeroSequenceImpedence+(3*faultImpedence)));
+        float imp1=negativeSequenceImpedence+zeroSequenceImpedence+(3*faultImpedence);
+        float equivalentImpedence=positiveSequenceImpedence+(imp/imp1);
+        float positiveCurrent=emf/equivalentImpedence;
+        float imp3=(negativeSequenceImpedence+(zeroSequenceImpedence+negativeSequenceImpedence+(3*faultImpedence)));
+        equivalentImpedence=negativeSequenceImpedence/imp3;
+
+        faultCurrent=3*(positiveCurrent*equivalentImpedence);
         return faultCurrent;
     }
 
